03cf3a945b2323ed227d20652ff0a0dc8d251b83
Hamster Tian <haotia@gmail.com>
2021-04-05 22:36:19 +0800

    dragon: update audio HIDL to version 6.0
    Don't know why...

--- device.mk
+++ device.mk
@@ -280,9 +280,9 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl:32 \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl:32 \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.service \
+    android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32
 
 PRODUCT_PACKAGES += \
